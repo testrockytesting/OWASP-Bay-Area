469ebb3ceb0b7432ca98117fe308fb59141748a3
sushi2k <sven@bsddaemon.org>
2018-10-15 10:56:06 -0700

    Adding apkx

---
+++ android/apkx/install.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+if [[ $EUID > 0 ]]; then 
+  echo "The installation script needs to be run as root."
+  exit 1
+else
+  cp apkx apkx-libs.jar enjarify.pex /usr/local/bin  
+  chmod 755 /usr/local/bin/apkx /usr/local/bin/enjarify.pex
+fi
